ac0a2e6ff5b9aa9b80c94b00f954617e6f755d81
SushantKrMishra <Sushant1768@gmail.com>
2023-08-03 13:58:56 +0530

    changes before uploading to heroku

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 const cookieParser = require("cookie-parser");
 if (process.env.NODE_ENV !== "production") {
@@ -16,4 +17,8 @@ const user = require("./routes/user");
 //Using Routes
 app.use("/api/v1", post);
 app.use("/api/v1", user);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 module.exports = app;
